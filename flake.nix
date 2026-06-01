3dd1460a605ff4d17e5524037fc33dd15b42f9d0
Renato Zannon <renato@rrsz.com.br>
2026-06-01 11:11:36 -0300

    refactor: simplify flake.nix output expressions

--- flake.nix
+++ flake.nix
@@ -12,6 +12,24 @@
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      mkCraneLib = pkgs:
+        (crane.mkLib pkgs).overrideToolchain (
+          pkgs.rust-bin.stable.latest.default.override {
+            targets = [ "x86_64-unknown-linux-musl" ];
+          }
+        );
+
+      mkPackages = pkgs:
+        let
+          craneLib = mkCraneLib pkgs;
+        in
+        {
+          msb = pkgs.callPackage ./pkgs/msb {
+            inherit craneLib;
+          };
+          msb-prebuilt = pkgs.callPackage ./pkgs/msb-prebuilt { };
+        };
     in
     {
       packages = forAllSystems (system:
@@ -20,30 +38,12 @@
             inherit system;
             overlays = [ (import rust-overlay) ];
           };
-          craneLib = (crane.mkLib pkgs).overrideToolchain (
-            pkgs.rust-bin.stable.latest.default.override {
-              targets = [ "x86_64-unknown-linux-musl" ];
-            }
-          );
-          msb = pkgs.callPackage ./pkgs/msb {
-            inherit craneLib;
-          };
-          msb-prebuilt = pkgs.callPackage ./pkgs/msb-prebuilt { };
+          p = mkPackages pkgs;
         in
-        {
-          inherit msb msb-prebuilt;
-          default = msb;
+        p // {
+          default = p.msb;
         });
 
-      overlays.default = final: _prev: {
-        msb = final.callPackage ./pkgs/msb {
-          craneLib = (crane.mkLib final).overrideToolchain (
-            final.rust-bin.stable.latest.default.override {
-              targets = [ "x86_64-unknown-linux-musl" ];
-            }
-          );
-        };
-        msb-prebuilt = final.callPackage ./pkgs/msb-prebuilt { };
-      };
+      overlays.default = final: _prev: mkPackages final;
     };
 }
