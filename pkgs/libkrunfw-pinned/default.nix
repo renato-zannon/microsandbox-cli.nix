a18c46b5d2d20010a85a9b08d25ffa5bbbc8e915
Renato Zannon <renato@rrsz.com.br>
2026-06-13 22:07:07 -0300

    Support building against newer nixpkgs

--- pkgs/libkrunfw-pinned/default.nix
+++ pkgs/libkrunfw-pinned/default.nix
@@ -1,17 +1,26 @@
 { libkrunfw, fetchFromGitHub, fetchurl }:
 
-libkrunfw.overrideAttrs (_old: rec {
-  version = "5.2.1";
-
-  src = fetchFromGitHub {
-    owner = "containers";
-    repo = "libkrunfw";
-    tag = "v${version}";
-    hash = "sha256-hRu9HEWTyToqntDkqBIvWEn+kAidQdspyWc6Le587qw=";
-  };
+let
+  kernelVersion = "6.12.68";
 
   kernelSrc = fetchurl {
-    url = "mirror://kernel/linux/kernel/v6.x/linux-6.12.68.tar.xz";
+    url = "mirror://kernel/linux/kernel/v6.x/linux-${kernelVersion}.tar.xz";
     hash = "sha256-02fHUEvU2lIN0B6wgSXS0KwIi8ivTNVtI28gdN1CJbc=";
   };
-})
+in
+  libkrunfw.overrideAttrs (_old: rec {
+    version = "5.2.1";
+    inherit kernelSrc;
+
+    src = fetchFromGitHub {
+      owner = "containers";
+      repo = "libkrunfw";
+      tag = "v${version}";
+      hash = "sha256-hRu9HEWTyToqntDkqBIvWEn+kAidQdspyWc6Le587qw=";
+    };
+
+    postPatch = ''
+      substituteInPlace Makefile \
+        --replace-fail 'curl $(KERNEL_REMOTE) -o $(KERNEL_TARBALL)' 'ln -s ${kernelSrc} $(KERNEL_TARBALL)'
+    '';
+  })
